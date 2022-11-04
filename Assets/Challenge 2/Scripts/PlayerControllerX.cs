ca61f146194b22cca37e183863217c11f9050277
frtobarn <frtobarn@unal.edu.co>
2022-11-04 03:49:27 -0500

    Challenge 2 finished

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    //counter for seconds
+    private float time = 3;
 
     // Update is called once per frame
     void Update()
     {
+        //increase time
+        time += Time.deltaTime;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && time > 2)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //reset time
+            time = 0;
         }
     }
 }
